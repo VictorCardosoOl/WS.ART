838a5e6a0b06b719ab2fb6c7942c855fdd1372ed
Victor Cardoso Cunha <105755943+VictorCardosoOl@users.noreply.github.com>
2026-02-08 09:43:54 -0300

    refactor: Remove unused components
    Removes the following components that are no longer in use:
    - SectionTitle
    - Navbar
    - Hero
    - About
    - Portfolio

--- components/FluidBackground.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// Componente descontinuado após remoção de Three.js
-// Mantido apenas para evitar erros de importação até que todas as referências sejam removidas
-const FluidBackground: React.FC = () => {
-  return null;
-};
-
-export default FluidBackground;
